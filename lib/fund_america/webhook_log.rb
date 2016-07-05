f4162695a9fbda5c7ca4c8d0c9a12d26cca44e43
Johan Oskarsson <johan@oskarsson.nu>
2016-07-05 10:11:29 -0700

    Merge from upstream

--- lib/fund_america/webhook_log.rb
+++ lib/fund_america/webhook_log.rb
@@ -9,16 +9,16 @@ module FundAmerica
       #  * page - page number of results; starts at 1, not 0
       #  * per - number of results per page
       def list(page: 1, per: 25)
-        request_uri = FundAmerica.base_uri + 'webhook_logs'
-        request_uri += "/?page=#{page}&per=#{per}" if page
-        API::request(:get, request_uri)
+        request_path = "webhook_logs"
+        request_path += "/?page=#{page}&per=#{per}" if page
+        API::request(:get, request_path)
       end
 
       # End point: https://apps.fundamerica.com/api/webhook_logs/:id (GET)
       # Usage: FundAmerica::WebhookLog.details(webhook_log_id)
       # Output: Returns the details of an webhook_log with matching id
       def details(webhook_log_id)
-        API::request(:get, FundAmerica.base_uri + "webhook_logs/#{webhook_log_id}")
+        API::request(:get, "webhook_logs/#{webhook_log_id}")
       end
 
     end
