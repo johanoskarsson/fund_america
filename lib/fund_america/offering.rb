50829117f58f2ddf8ab1e631e84585632a8af760
David Reid <dreid@dreid.org>
2016-07-19 11:32:55 -0700

    precedence.

--- lib/fund_america/offering.rb
+++ lib/fund_america/offering.rb
@@ -21,7 +21,7 @@ module FundAmerica
       # Output: Updates an offering
       # In sandbox mode accept_investments can only be updated
       def update(offering_id, options)
-        end_point_url = FundAmerica.mode == 'sandbox' ? 'test_mode/' : '' + "offerings/#{offering_id}"
+        end_point_url = (FundAmerica.mode == 'sandbox' ? 'test_mode/' : '') + "offerings/#{offering_id}"
         API::request(:patch, end_point_url, options)
       end
 
